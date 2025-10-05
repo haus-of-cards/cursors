fd19be96f2de3697761e5e64c30ee596b1227dca
ddean11 <99177938+ddean11@users.noreply.github.com>
2025-10-05 12:11:48 +1100

    Styling for Docs page

--- frontend/src/pages/DocoPage.css.ts
+++ frontend/src/pages/DocoPage.css.ts
@@ -8,35 +8,46 @@ export const docopage = style({
   // display: "grid",
   display: "flex",
   flexDirection: "column",
+
   alignItems: "center"
 });
 
 export const docopageMenuDiv = style({
+  // Styling as a Flex Item
+  flexBasis: "600px",
+  flexGrow: "1",
+  flexShrink: "1",
+  
+  
+  // Styling as a Flex Container
   marginTop: "20px",
   padding: "20px",
   display: "flex",
   flexDirection: "row",
-  height: "200px",
+  // height: "200px",
   // justifyContent: "center",
   // justifyContent: "flex-start",
   
 });
 
 export const docopageMenubar = style({
-  marginLeft: "10px",
-  flexBasis: "200px",
-  flexGrow: "0",
-  flexShrink: "0",
+  marginLeft: "20px",
+  // maxWidth: "200px",
+  width: "200px",
+  // flexBasis: "200px",
+  // flexGrow: "0",
+  // flexShrink: "0",
   alignSelf: "center"
 });
 
 export const docopageMenuContent = style({
-  marginLeft: "10px",
-  maxWidth: "800px",
-  flex: 1
+  marginLeft: "100px",
+  maxWidth: "600px",
+  flex: 1,
   // flexBasis: "200px",
   // flexGrow: "0",
   // flexShrink: "0",
   // border: "solid"
+  alignSelf: "center"
 });
 

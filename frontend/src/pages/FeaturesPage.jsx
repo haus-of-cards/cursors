797dbe4214c472143ae220923804cd789df5c9fe
ddean11 <99177938+ddean11@users.noreply.github.com>
2025-10-29 00:05:18 +1100

    Enabled the showSystemCursor option for the Demo Cursor

--- frontend/src/pages/FeaturesPage.jsx
+++ frontend/src/pages/FeaturesPage.jsx
@@ -18,7 +18,23 @@ import CursorOptions from '../components/features/demo/CursorOptions';
 // React component FeaturesPage
 export default function FeaturesPage({setMainCursor}) { 
   // States
-  const [demoCursor, setDemoCursor] = useState(false); // Customised demo cursor not displayed by default  
+  
+    // Demo Cursor
+    // const [demoCursor, setDemoCursor] = useState(false); // Customised demo cursor not displayed by default 
+    const [demoCursor, setDemoCursor] = useState({
+      enable: false, // Customised demo cursor not displayed by default
+      layers: [ { fill: "yellow", stroke: "black", strokeSize: 10, size: { height: 20, width: 20 } } ],
+      showSystemCursor: true,
+      // showSystemCursor: false,
+      mixBlendMode: "normal",
+      zIndex: 20,
+      ignoreAccessibility: true,
+      // effects,
+      // hoverSelector = 'a, button, [role="button"], input, textarea, select',
+    }); 
+  
+  
+
   const [testBed, setTestBed] = useState(false); // Our Testbed is initially off
   // const [numberOfLayers, setNumberOfLayers] = useState(1); // Initially only one layer for the demo cursor
   // const [layers, setLayers] = useState([]); // Used tohold the layer options for each layer
@@ -29,17 +45,21 @@ export default function FeaturesPage({setMainCursor}) {
     function handleCursorOverTestbed(){
       if(testBed){
         setMainCursor(false);
-        setDemoCursor(true);
+        // setDemoCursor({ enable: true }); *** Note: cannot update directly - need to copy existing state first!
+        setDemoCursor({ ...demoCursor, enable: true }); // Spread operator to copy the other fields
       } else {
         setMainCursor(true);
-        setDemoCursor(false);
+        // setDemoCursor({ enable: false });
+        setDemoCursor({ ...demoCursor, enable: false }); // Spread operator to copy the other fields
       }
     }
     
     // Handle cursor leaving the Testbed ie. only main cursor shows
     function handleCursorLeavingTestbed(){  
-        setDemoCursor(false);
-        setMainCursor(true);
+      // setDemoCursor(false);
+      // setDemoCursor({ enable: false });
+      setDemoCursor({ ...demoCursor, enable: false }); // Spread operator to copy the other fields
+      setMainCursor(true);
     }
 
 
@@ -48,10 +68,20 @@ export default function FeaturesPage({setMainCursor}) {
   return (
     <div className={styles.featuresPage}>
       {/* Demo Cursor. *** Should this go here or inside the Testbed?  */}
-      <ReactCursor
+      {/* <ReactCursor 
         enable={demoCursor}
         layers={ [ { fill: "red", stroke: "green", size: { height: 30, width: 30 } } ] } 
         zIndex={10}  
+      /> */}
+      <ReactCursor 
+        enable={demoCursor.enable}
+        // layers={ [ { fill: "yellow", stroke: "black", strokeSize: 10, size: { height: 20, width: 20 } } ] }
+        layers={demoCursor.layers} 
+        // zIndex={10}  
+        zIndex={demoCursor.zIndex} 
+        showSystemCursor={demoCursor.showSystemCursor} 
+        mixBlendMode={demoCursor.mixBlendMode} 
+        ignoreAccessibility={demoCursor.ignoreAccessibility} 
       />
       
       <h1 >Cursor Features Demo</h1>
@@ -72,7 +102,7 @@ export default function FeaturesPage({setMainCursor}) {
         </div>
 
         {/* Demo Cursor Options */}
-        <CursorOptions />              
+        <CursorOptions demoCursor={demoCursor} setDemoCursor={setDemoCursor} />              
       
       </div>
 

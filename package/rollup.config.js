d74619e79f8bc11c028ce14e2d54d89a9aeb7a56
marcobuontempo <marco.buon36@gmail.com>
2025-10-29 11:24:00 +1100

    Configure rollup and TS for build output

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -1,7 +1,9 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
 import svgr from "@svgr/rollup";
+import terser from "@rollup/plugin-terser";
 import packageJson from "./package.json" with { type: "json" };
 
 export default [
@@ -23,9 +25,31 @@ export default [
       resolve(),
       commonjs(),
       svgr(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+      }),
       typescript({
         tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts", "**/*.stories.tsx"],
+        exclude: [
+          "**/*.test.tsx",
+          "**/*.test.ts",
+          "**/*.stories.ts",
+          "**/*.stories.tsx",
+        ],
+      }),
+      terser({
+        compress: {
+          drop_console: true, // Keep console logs for user debugging
+          dead_code: true, // Remove unreachable code
+          drop_debugger: true, // Remove debugger statements
+        },
+        mangle: {
+          reserved: ["ReactCursor"], // Don't mangle main export name
+        },
+        format: {
+          comments: false, // Remove all comments
+        },
       }),
     ],
     external: ["react", "react-dom"],
